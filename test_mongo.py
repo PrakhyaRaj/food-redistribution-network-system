44045d83f562d158a1a8aad3fd5fcb357aec1d64
PrakhyaRaj <prakhya.raj20@gmail.com>
2025-12-31 08:51:16 +0530

    Deleted Unnecessary Files

--- test_mongo.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-try:
-    client = MongoClient(
-        "mongodb://myUser:MyPass123@localhost:27017/?authSource=admin"
-    )
-    print("Connected! Databases:", client.list_database_names())
-except Exception as e:
-    print("Auth failed:", e)
